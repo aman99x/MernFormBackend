17d349cb44985250c25c3d156c61220f97eb83b5
aman99x <aman88sh@gmail.com>
2024-10-18 12:28:10 +0530

    update

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://soft-alfajores-51ccf5.netlify.app"],
+  credentials:true
+}
+app.use(cors(corsOptions));
 
 // Connect Database
 connectDB();
